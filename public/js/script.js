401b20058727c8c094150d6a8381f36f7742290e
Esteban Rojas <estered9112@gmail.com>
2017-05-03 16:58:21 -0400

    added general css changes, moved keyframes to utils sass folder

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const scrollevt = () => {
-    let navbr = document.querySelector('#navmenu');
-    if (window.pageYOffset > 320) {
-      navbr.classList.remove('e-nav-transparent')
-    } else {
-      navbr.classList.add('e-nav-transparent')
-    }
-  };
-  window.onscroll = scrollevt;
+$(document).ready(function() {
+  //Navbar Animation functions
+  $(window).scroll(function() {
+    yPos();
+  });
 
-})
+  function yPos() {
+    if ($(window).scrollTop() > yHeight) {
+      $('#navmenu').removeClass('e-nav-transparent', {duration: 500});
+    } else {
+      $('#navmenu').addClass('e-nav-transparent', {duration: 500});
+    }
+  }
+
+  var yHeight = $('.e-header').position().top + $('.e-header').outerHeight() - $('#navmenu').outerHeight();
+
+  //Smooth scrolling to anchor
+  $(document).on('click', '#navmenu a', function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 700);
+  });
+  
+});
