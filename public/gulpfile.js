8a494ca5958f75b958b59637856e3b108d50810b
Esteban Rojas <estered9112@gmail.com>
2017-05-04 01:02:22 -0400

    added gulp instead of compass to compile and watch sass files

---
+++ public/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//convert sass to css
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
