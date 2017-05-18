537dcb67d2ae040768507271092183e4b28fb5e1
Esteban Rojas <estered9112@gmail.com>
2017-05-18 11:18:13 -0400

    added gulp uglify and gulp minify-css to the project

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,14 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
 //convert sass to css
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
 });
+gulp.task('js', function(){
+   gulp.src('js/*.js')
+   .pipe(concat('script.js'))
+   .pipe(uglify())
+   .pipe(gulp.dest('build/'));
+});
 
+gulp.task('css', function(){
+   gulp.src('css/*.css')
+   .pipe(concat('styles.css'))
+   .pipe(minify())
+   .pipe(gulp.dest('build/'));
+});
 //Watch task
-gulp.task('default',function() {
+gulp.task('default', function(){
     gulp.watch('sass/**/*.scss',['styles']);
+});
+gulp.task('default',['js','css'],function(){
 });
