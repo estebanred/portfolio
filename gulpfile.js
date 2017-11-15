91b4deb3582247da5095f70030e8928992b744e4
Esteban Rojas <estered9112@gmail.com>
2017-11-15 16:14:01 -0500

    Fixed gulpfile and added browser sync

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+let cleanCSS = require("gulp-clean-css");
+var browserSync = require("browser-sync");
+
+//configure browser-sync
+gulp.task("browser-sync", function () {
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./public/sass/*.scss", ["sass"]);
+    gulp.watch("./public/js/**/*.js", ["minifyjs"]);
+    gulp.watch("./*.html").on("change", browserSync.reload);
+});
+
+//convert sass to css
+gulp.task("sass", function () {
+    return gulp.src("./public/sass/*.scss")
+        .pipe(sass())
+        .pipe(cleanCSS({ compatibility: "ie8" }))
+        .pipe(gulp.dest("./public/build"))
+        .pipe(browserSync.stream());
+});
+
+//minify js
+gulp.task("minifyjs", function () {
+    return gulp.src("./public/js/**/*.js")
+        .pipe(concat("script.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("./public/build"))
+        .pipe(browserSync.stream());
+});
+
+
+// minify css and js
+gulp.task("default", ["minifyjs", "sass", "browser-sync"], function () {
+    gulp.watch("./public/sass/**/*.scss", ["sass"]);
+    gulp.watch("./public/js/**/*.js", ["minifyjs"]);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $(window).scroll(function() {
+$(document).ready(function () {
+  $(window).scroll(function () {
     yPos();
   });
 
@@ -14,11 +14,10 @@ $(document).ready(function() {
   }
 
   //Smooth scrolling to anchor
-  $(document).on('click', '.anchor-link', function(event) {
+  $(document).on('click', '.anchor-link', function (event) {
     event.preventDefault();
     $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
     }, 480);
   });
-
 });
